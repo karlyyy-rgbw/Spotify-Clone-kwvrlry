36cb360884979bed1a73cbe43b181392826b32a3
karlyyy-rgbw <kvsuayan@gbox.ncf.edu.ph>
2024-12-15 10:37:03 +0800

    final touches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
       <div className='flex items-center gap-4 pr-3'>
         <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
         <p className='bg-black py-1 px-4 rounded-2xl text-white text-[15px] cursor-pointer hover:bg-[#ffffff26]' onClick={handleLogout}>Log Out</p>
-        <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex justify-center'>o</p>  
+        <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex justify-center'>{user.email.charAt(0)}</p>  
       </div>
     </div>
     <div className='flex items-center gap-2 mt-4 pl-2'>
